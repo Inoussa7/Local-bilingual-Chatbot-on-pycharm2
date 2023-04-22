cd7e7c20bfcf99d45695cd012c1ae0b1523ab0d6
Inoussa7 <126445891+Inoussa7@users.noreply.github.com>
2023-04-22 14:55:35 -0500

    Update Language app Fr_En-Pyscharm.py

--- Language app Fr_En-Pyscharm.py
+++ Language app Fr_En-Pyscharm.py
@@ -8,7 +8,7 @@ import string
 import random
 from playsound import playsound
 import time
-openai.api_key = "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"
+openai.api_key = "YOUR_API_KEY_HERE"
 
 def translate(text, target_language):
     response = openai.Completion.create(
@@ -57,7 +57,7 @@ def generate_text(prompt, language):
     return message
 
 # Set up OpenAI API credentials
-openai.api_key = "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"
+openai.api_key = "YOUR_API_KEY_HERE"
 
 def generate_response(text, language,processed_text):
     # Define the GPT-3.5 model to use based on language
